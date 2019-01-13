b48cf25a47cd2e3abcb875447bf05b60739398fe
Fernando Aguilar <fernando@aguilar.net.br>
2019-01-13 18:55:21 -0300

    Creating Colorscheme for app

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'login.dart';
+import 'colors.dart';
 
+final ThemeData _kShrineTheme = _buildShrineTheme("blue");
 
 class InsaneApp extends StatelessWidget{
   @override
@@ -11,6 +13,7 @@ class InsaneApp extends StatelessWidget{
       home: HomePage(),
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
+      theme: _kShrineTheme,
     );
   }
 
@@ -25,6 +28,40 @@ class InsaneApp extends StatelessWidget{
     );
   }
 }
+
+ThemeData _buildShrineTheme(String color) {
+  final ThemeData base = ThemeData.light();
+
+  if (color == "blue") {
+    return base.copyWith(
+      accentColor: kShrineIndigo900,
+      primaryColor: kShrineBlue100,
+      buttonTheme: base.buttonTheme.copyWith(
+        buttonColor: kShrineBlue100,
+        textTheme: ButtonTextTheme.normal,
+      ),
+      scaffoldBackgroundColor: kShrineBackgroundWhite,
+      cardColor: kShrineBackgroundWhite,
+      textSelectionColor: kShrineBlue100,
+      errorColor: kShrineErrorIndigo,
+    );
+  }
+  else {
+    return base.copyWith(
+    accentColor: kShrineBrown900,
+    primaryColor: kShrinePink100,
+    buttonTheme: base.buttonTheme.copyWith(
+      buttonColor: kShrinePink100,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    scaffoldBackgroundColor: kShrineBackgroundWhite,
+    cardColor: kShrineBackgroundWhite,
+    textSelectionColor: kShrinePink100,
+    errorColor: kShrineErrorRed,
+    );
+  }
+}
+
 // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
 // TODO: Make currentCategory field take _currentCategory (104)
 // TODO: Pass _currentCategory for frontLayer (104)
--- lib/colors.dart
+++ lib/colors.dart
@@ -11,3 +11,13 @@ const kShrineErrorRed = const Color(0xFFC5032B);
 
 const kShrineSurfaceWhite = const Color(0xFFFFFBFA);
 const kShrineBackgroundWhite = Colors.white;
+
+// Custom Colors Light Blue
+const kShrineBlue50 = const Color(0xFFe0f7fa);
+const kShrineBlue100 = const Color(0xFFb2ebf2);
+const kShrineBlue300 = const Color(0xFFb2ebf2);
+const kShrineBlue400 = const Color(0xFF81b9bf);
+
+const kShrineIndigo900 = const Color(0xFF9fa8da);
+
+const kShrineErrorIndigo = const Color(0xFFC5032B);
