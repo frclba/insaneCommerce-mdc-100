1339a158b4b6c0da50ce018c7b402c96e41c96e3
Fernando Aguilar <fernando@aguilar.net.br>
2019-01-22 17:28:33 -0300

    Changing listview to asymmetric view

--- lib/home.dart
+++ lib/home.dart
@@ -1,78 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'model/products_repository.dart';
 import 'model/product.dart';
-import 'app.dart';
+import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
   final Category category;
   const HomePage({this.category: Category.all});
 
-  // TODO -> FIX Build to pass a category and return only its products
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(context),
+    return AsymmetricView(
+      products: ProductsRepository.loadProducts(category)
     );
-  }
-
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-      
-    if(products == null || products.isEmpty){
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-      locale: Localizations.localeOf(context).toString()
-    );
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        elevation: 0.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18/11,
-              child: Image.asset(
-                product.assetName,
-                fit: BoxFit.fitWidth,
-                package: product.assetPackage,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      product == null ? '' : product.name,
-                      style: theme.textTheme.button,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    SizedBox(height: 4.0),
-                    Text(
-                      product == null ? '' : formatter.format(product.price),
-                      style: theme.textTheme.caption,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
   }
 }
